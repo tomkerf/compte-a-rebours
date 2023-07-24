bb0ed3dec410c8e3fdc19d07a52321497a4bbb43
tomkerf <tomkerf@gmail.com>
2023-07-24 13:28:18 +0200

    1er envoi

---
+++ index.js
@@ -0,0 +1,30 @@
+let totalSeconds;
+let interval;
+
+function countDown() {
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+  const sec = seconds < 10 ? '0' + seconds : seconds;
+
+  countdownDisplay.textContent = `${minutes} : ${sec}`;
+
+  if (totalSeconds > 0) {
+    totalSeconds--;
+  } else {
+    countdownDisplay.textContent = 'Booom !!!';
+    clearInterval(interval);
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (isNaN(choice.value)) {
+    alert('Veuillez entrer un chiffre svp');
+  } else {
+    totalSeconds = choice.value * 60;
+    choice.value = '';
+    clearInterval(interval);
+    interval = setInterval(countDown, 1000);
+  }
+});
